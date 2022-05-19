7edb91d427da85e5424488257aaae3b3b6b10bae
abdielbatista <abdielb@yahoo.com>
2022-05-18 21:06:34 -0300

    config db

--- src/database/migrations/20220517232529-create-nome.js
+++ src/database/migrations/20220517232529-create-nome.js
@@ -13,7 +13,7 @@ module.exports = {
           type: Sequelize.STRING,
           allowNull: false
         },
-        senha:{
+        senha:{ 
           type: Sequelize.INTEGER,
           allowNull: false
         },
